aab40852adfd75a3e4f6aa1c2c6b9959113cd18c
winebarrel <sugawara@winebarrel.jp>
2024-01-10 16:59:32 +0900

    Add myNotifications call

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,5 +5,22 @@ let env = ProcessInfo.processInfo.environment
 let token = env["GITHUB_TOKEN"]!
 let config = TokenConfiguration(token)
 
-let user = try! await Octokit(config).me()
+let octokit = Octokit(config)
+let user = try! await octokit.me()
 print(user.name!)
+
+// async未対応の場合
+let ns = try! await withCheckedThrowingContinuation { continuation in
+    octokit.myNotifications { response in
+        switch response {
+        case let .success(n):
+            continuation.resume(returning: n)
+        case let .failure(error):
+            continuation.resume(throwing: error)
+        }
+    }
+}
+
+for n in ns {
+    print(n.subject.title!)
+}
